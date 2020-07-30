76beb8426eec41647c309295f60bacc6d9c80f5c
Dan Swan <rodger.swan@gmail.com>
2020-07-30 16:18:22 +1000

    add express.static to fix css link issue

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + "/public"));
 
 
 require("./routes/apiRoutes")(app);
