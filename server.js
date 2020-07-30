42418c3e465b8e86070acada4b0ae5dec23a6199
Dan Swan <rodger.swan@gmail.com>
2020-07-30 18:35:02 +1000

    server code completed and working

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
@@ -6,12 +6,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
-
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-
-const port = process.env.PORT || 3030
+const port = process.env.PORT || 3030;
 app.listen(port, () => {
-    console.log(`Server started on ${port}`);
+  console.log(`Server started on ${port}`);
 });
