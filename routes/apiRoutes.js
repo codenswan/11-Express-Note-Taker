a9670087f9d6bef7b093dd8bf73ba467b4124a17
Dan Swan <rodger.swan@gmail.com>
2020-08-03 13:09:06 +1000

    bug fix for filter method

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,4 @@
-const notes = require("../db/db.json");
+let notes = require("../db/db.json");
 const path = require("path");
 const fs = require("fs");
 const uniqueID = require("uniqid");
@@ -27,22 +27,18 @@ module.exports = (app) => {
   });
 
   app.delete("/api/notes/:id", (req, res) => {
-    const noteIdToDelete = req.params.id;
-
-    let allTheNotes = JSON.parse(fs.readFileSync(dir, "utf-8")) || [];
-
-    const filtered = allTheNotes.filter((note) => {
-      const leaveNote = note.id !== noteIdToDelete;
-      return leaveNote;
-    });
-
     try {
+      const noteIdToDelete = req.params.id;
+
+      const filtered = notes.filter((note) => note.id != noteIdToDelete);
+      notes = filtered
+      
       fs.writeFileSync(dir, JSON.stringify(filtered));
       res.json(filtered);
     } catch (err) {
       console.log(err);
       res.status(400).json({
-      message: err,
+        message: err,
       });
     }
   });
