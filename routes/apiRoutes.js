7d39546e1cf15fa325785a506a2cb1fe5ba79b8f
Dan Swan <rodger.swan@gmail.com>
2020-08-03 10:38:26 +1000

    delete route working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,7 @@ const notes = require("../db/db.json");
 const path = require("path");
 const fs = require("fs");
 const uniqueID = require("uniqid");
+const dir = path.join(__dirname, "../db/db.json");
 
 module.exports = (app) => {
   app.get("/api/notes", (req, res) => {
@@ -13,17 +14,36 @@ module.exports = (app) => {
     newNote.id = uniqueID();
     notes.push(newNote);
 
-    const dir = path.join(__dirname, "../db/db.json");
     const data = JSON.stringify(notes, null, 2);
-    fs.writeFileSync(dir, data, (err) => {
-      if (err) throw err
-      console.log("New note added");
-    });
-    res.json(notes)
+    try {
+      fs.writeFileSync(dir, data);
+      res.json(notes);
+    } catch (err) {
+      console.log(err);
+      res.status(400).json({
+        message: err,
+      });
+    }
   });
 
-  app.delete("api/notes/:id", (req, res) => {
-    const note = req.params.id
-    console.log(note);
-  })
+  app.delete("/api/notes/:id", (req, res) => {
+    const noteIdToDelete = req.params.id;
+
+    let allTheNotes = JSON.parse(fs.readFileSync(dir, "utf-8")) || [];
+
+    const filtered = allTheNotes.filter((note) => {
+      const leaveNote = note.id !== noteIdToDelete;
+      return leaveNote;
+    });
+
+    try {
+      fs.writeFileSync(dir, JSON.stringify(filtered));
+      res.json(filtered);
+    } catch (err) {
+      console.log(err);
+      res.status(400).json({
+      message: err,
+      });
+    }
+  });
 };
